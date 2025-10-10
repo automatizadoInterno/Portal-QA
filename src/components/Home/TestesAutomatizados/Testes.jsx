6be5533765da68fb0ed8814da761022e4b0aaeb8
baestero <leonardobaestero@gmail.com>
2025-10-10 15:29:18 -0300

    adicionado links para testes de api

--- src/components/Home/TestesAutomatizados/Testes.jsx
+++ src/components/Home/TestesAutomatizados/Testes.jsx
@@ -7,10 +7,18 @@ const Testes = () => {
       <h1 className={styles.title}>Relatórios de Testes Automatizados</h1>
       <div className={styles.cardContainer}>
         <div className={styles.card}>
-          <h2>API</h2>
+          <a
+            href="https://automatizadointerno.github.io/relatorios-api/"
+            target="blank"
+          >
+            {" "}
+            <h2>API</h2>
+            <p>Relatórios dos ultimos testes de API executados via Pipeline</p>
+          </a>
         </div>
         <div className={styles.card}>
           <h2>WEB</h2>
+          <p>Relatórios dos ultimos testes de API executados via Pipeline</p>
         </div>
       </div>
     </div>
